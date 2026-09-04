f7cb3af982f2c9fc52918562243f83e541db50c4
Florenso <aminamrouch5@gmail.com>
2026-09-04 20:20:58 +0100

    Refactor Calc function for better readability

--- Tableux/Exercice3.js
+++ Tableux/Exercice3.js
@@ -2,11 +2,14 @@ var prompt = require('prompt-sync')();
 var index = prompt('quelle est index de tableau : ');
 const numbers = []
 function Calc(index){
-let somme = 0;
-for(i=0;i<=index-1;i++){
-    numbers[i]=Number(prompt(`Order ${i+1} : `))
-}
-for (number of numbers){
-    somme+=number;
-   console.log(somme)}}
+    let somme = 0;
+    for(i=0;i<=index-1;i++)
+    {
+        numbers[i]=Number(prompt(`Order ${i+1} : `))
+    }
+    for (number of numbers){
+        somme+=number;
+       console.log(somme)    
+                           }
+                    }
 Calc(index)
