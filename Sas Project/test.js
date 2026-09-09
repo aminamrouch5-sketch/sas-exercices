a0098269535877187e80d954b9675d314ff1adc9
Florenso <aminamrouch5@gmail.com>
2026-09-09 21:14:44 +0200

    Update test.js
    crier un draft pout le function Recherché est je test cet draft qui été bien ou no

--- Sas Project/test.js
+++ Sas Project/test.js
@@ -1,8 +1,45 @@
-var prompt = require('prompt-sync')();
-let name = parseInt(prompt('hi'))
-console.log(name)
-console.log(typeof(1))
-let dd = []
-console.log(dd.length)
-let DisponsibleIDs=[1];
-console.log(DisponsibleIDs.length)
+
+function Recherche(){
+    console.log("========================================")
+    console.log("=  Recharche par Nom :  [0] | Id : [1] =")
+    console.log("=                           |          =")
+    console.log("========================================")
+    let order = parseInt(prompt("Choisir Votre Choice : "))
+    let isFound=false;
+    if (!isNaN(order)){
+        if (order==0){
+            let name = prompt("Write the name : ").toUpperCase()
+            if (name.length<3 && name.length>8){
+            for (i=0;i<tickets.length;i++){
+                if (tickets[i].name==name){
+                    isFound=true;
+                    console.log(`___________________________________________________________________________________\n
+                                                                TICKET EXISTE                                      `)
+                    console.log(`__________________________________________________________________________________\n\n\n\n`)
+                    console.log(`console.log
+    | TICKET #${tickets[i].id}
+    |  Passenger Name : ${tickets[i].passengerName}
+    |  Seat Number    : ${tickets[i].seatNumber}
+    |  Trip ID        : ${tickets[i].tripId }
+    |  Price          : ${tickets[i].price}
+    |  Depart         : ${tickets[i].departure}
+    |  Destination    : ${tickets[i].destination}
+    |  Place          : ${tickets[i].place}
+    
+       `)
+        break
+                    
+
+                }
+            }
+            if (!isFound){
+                console.log("========================= TICKET NA PAS EXISTE ================================")
+
+            }
+        }
+    }
+    }else{
+        console.log("S'il te plait insert un valid nom ")
+    }
+
+}
