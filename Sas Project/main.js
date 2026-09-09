112f72fbffa1e3b84f3b3bad175f36866084d260
Florenso <aminamrouch5@gmail.com>
2026-09-09 21:48:40 +0200

    Il'y a un issue dans function recherche mes je sauvit it pout finish le project dans un autre pc

--- Sas Project/main.js
+++ Sas Project/main.js
@@ -122,41 +122,52 @@ AfficherTicket:function(){
     
     }
 },
-Annule:function(){
-    let isFound = true;
-    let TicketID = parseInt(prompt("Insert le ticket id que tu peux annule : "))
-    // IF USER INSERT A BAD INPUT LIKE STRING OR EMPTY SPACE 
-    if (!isNaN(TicketID)){
-        for ( i in tickets){
-            if (TicketID==tickets[i].id){
-                console.log(`===========Le billet a été supprimé avec succès=============`)
-                places[tickets[i].tripId].place--
-                delete tickets[i];
-                DisponsibleIDs[index]=TicketID
-                isFound = true;
-                index++;
-                
-                               break;
-            }
-            else {
-                isFound=false;
-            }
-        }
-        if(!isFound){
-            console.log('======== TICKET N PAS EXISTE ========')
-        }
-    }
-    else{
-        console.log('Insert valid ticketid ....')
-    }
+Recherche:function(){
+    console.log("========================================")
+    console.log("=  Recharche par Nom :  [0] | Id : [1] =")
+    console.log("=                           |          =")
+    console.log("========================================")
+    let order = parseInt(prompt("Choisir Votre Choice : "))
+    let isFound=false;
+    if (!isNaN(order)){
+        if (order==0){
+            let name = prompt("Write the name : ").toUpperCase()
+            if (name.length<3 && name.length>8){
+            for (i=0;i<tickets.length;i++){
+                if (tickets[i].passengerName.toUpperCase()==name){
+                    isFound=true;
+                    console.log(`___________________________________________________________________________________\n
+                                                                TICKET EXISTE                                      `)
+                    console.log(`__________________________________________________________________________________\n\n\n\n`)
+                    console.log(`console.log
+    | TICKET #${tickets[i].id}
+    |  Passenger Name : ${tickets[i].passengerName}
+    |  Seat Number    : ${tickets[i].seatNumber}
+    |  Trip ID        : ${tickets[i].tripId }
+    |  Price          : ${tickets[i].price}
+    |  Depart         : ${tickets[i].departure}
+    |  Destination    : ${tickets[i].destination}
+    |  Place          : ${tickets[i].place}
     
+       `)
+        break
+                    
+
+                }
+            }
+            if (!isFound){
+                console.log("========================= TICKET NA PAS EXISTE ================================")
+
+            }
+        }
+    }
+    }else{
+        console.log("S'il te plait insert un valid nom ")
+    }
+
 }
+
 }
-
-
-
-
-
 
 
 
@@ -203,14 +214,27 @@ switch(choice){
     case "4":
         Manage.Annule();
         break;
-
     case "5":
-
-
+        Manage.Recherche();
+        break;
     case "6":
 
     case "7":
 
+    case "8":
+        console.log("=====================================")
+        console.log("= RAILWAY MANAGER V1                =")
+        console.log("=====================================")
+        console.log(`
+1. Afficher les trajets 
+2. Acheter un ticket 
+3. Afficher les tickets 
+4. Annuler un ticket 
+5. Rechercher un ticket 
+6. Filtrer les trajets 
+7. Trier les trajets 
+0. Quitter`)
+    break;
     default:
         console.log('insert un valid choice ...')
         break;
